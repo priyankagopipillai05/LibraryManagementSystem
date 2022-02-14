66f5725d3b85edbe5ead1538c571de27b0c35396
priyanka <priyankag@gmail.com>
2022-02-14 01:16:18 -0600

    Changes for db service

--- databaseService/src/main/java/com/library/management/databaseService/DatabaseServiceApplication.java
+++ databaseService/src/main/java/com/library/management/databaseService/DatabaseServiceApplication.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class DatabaseServiceApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Database starting");
 		SpringApplication.run(DatabaseServiceApplication.class, args);
 	}
 
