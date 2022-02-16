a422b4650fe994153af497cec860daaf671bd55e
priyanka <priyankag@gmail.com>
2022-02-16 01:24:44 -0600

    adding api gateway

---
+++ apiGateway/src/main/java/com/library/management/apiGateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.library.management.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
